77ba96277e7d8d70488b11dc657ab0a59c743b2f
passchaos <greedwolf.dss@gmail.com>
2025-09-19 03:32:51 +0800

    feat: add multiple plot for line figure

--- examples/line.rs
+++ examples/line.rs
@@ -1,11 +1,12 @@
 use plotiron::*;
 
 fn main() {
-    let x: Vec<_> = (-100..100).map(|i| (i as f64) * 0.1).collect();
+    let x: Vec<_> = (-100..100).map(|i| (i as f64) * 0.2).collect();
     let y: Vec<_> = x.iter().map(|a| a.sin()).collect();
+    let z: Vec<_> = x.iter().map(|a| a.cos()).collect();
 
-    let mut fig = figure();
-    fig.add_subplot().plot(&x, &y);
+    let mut fig = figure_with_size(1000.0, 800.0);
+    fig.add_subplot().plot(&x, &y).plot(&x, &z);
 
     std::fs::write("output/line.svg", fig.to_svg()).unwrap();
 
